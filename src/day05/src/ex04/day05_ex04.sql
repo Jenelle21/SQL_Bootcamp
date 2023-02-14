ea735d9482da3ebdca752b2d410c79d37e998f7a
jenellep <jenellep@student.21-school.ru>
2023-02-14 19:30:42 +0300

    First day of the second week

---
+++ src/day05/src/ex04/day05_ex04.sql
@@ -0,0 +1,9 @@
+CREATE UNIQUE INDEX idx_menu_unique ON menu (pizzeria_id, pizza_name);
+
+SET enable_seqscan = OFF;
+
+EXPLAIN ANALYZE
+SELECT *
+FROM menu
+WHERE pizzeria_id = 4
+  AND pizza_name = 'cheese pizza';
